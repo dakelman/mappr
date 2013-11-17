439ee0e3fe2094042de6a2b4a2ed6e41e32058ec
Diana Akelman <dakelman@dakelman-VirtualBox.(none)>
2013-11-17 15:56:25 -0500

    add form for the place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,4 +4,18 @@ class PlacesController < ApplicationController
 		@place = Place.last
 	end
 
+	def new
+		@place = Place.new
+	end
+
+	def create
+		@place = Place.create (place_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def place_params
+		params.require(:place).permit(:title, :address )
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Mappr::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
     root 'places#index'
-
+    resources :places
+    
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
